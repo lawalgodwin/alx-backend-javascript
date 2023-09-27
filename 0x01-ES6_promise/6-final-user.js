c116e0d48c90071b14732059cef41540a1e69f3a
lawalgodwin <godwinl200@gmail.com>
2023-09-27 09:44:45 +0100

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,5 +6,18 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   const uploadPhotoPromise = uploadPhoto(filename);
 
   return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
-    .then((data) => [...data]);
+    .then((values) => {
+      const response = [];
+      values.forEach((v) => {
+        if (v.status === 'fulfilled') {
+          response.push({ status: v.status, value: v.value });
+        } else {
+          response.push({
+            status: v.status,
+            value: v.reason,
+          });
+        }
+      });
+      return response;
+    });
 }
