8731e6a92ab663048e64366fe1d8bc9fc36bad71
lawalgodwin <godwinl200@gmail.com>
2023-09-27 09:14:05 +0100

    Handle returned Promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-    promise.then(() =>  ({status: 200, body: "Success",}))
-    .then(() => console.log("Got a response from the API"))
-    .then(undefined, error => error); // another way of writing .catch(err => err)
+  promise.then(() => ({ status: 200, body: 'Success' }))
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
 }
