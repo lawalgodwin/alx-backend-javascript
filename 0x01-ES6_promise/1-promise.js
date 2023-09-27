a2ad1dad66c775fd765dcd2ab1b7885c738955cd
lawalgodwin <godwinl200@gmail.com>
2023-09-27 08:57:06 +0100

    Resolve or regect based on status

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,10 @@
 export default function getFullResponseFromAPI(success) {
   if (success) {
     return Promise.resolve({
-        status: 200,
-        body: "Success",
-    })
+      status: 200,
+      body: 'Success',
+    });
   }
-  else {
-    return Promise.reject(Error("The fake API is not working currently"))
-  }
+
+  return Promise.reject(Error('The fake API is not working currently'));
 }
