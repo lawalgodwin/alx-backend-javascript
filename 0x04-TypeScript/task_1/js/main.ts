2a132ecac118855efb92ce25d65550bab84bc4b1
lawalgodwin <godwinl200@gmail.com>
2023-10-26 14:51:56 +0100

    Add solution to task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -33,3 +33,15 @@ const director1: Directors = {
 }
 
 console.log(director1)
+
+// Task 3
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) =>{
+    return `${firstName[0]}. ${lastName}`
+}
+
+console.log(printTeacher("John", "Doe"))
