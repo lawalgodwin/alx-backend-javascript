b960586dcae018e4bf5ca1a85de554034ce31ad6
lawalgodwin <godwinl200@gmail.com>
2023-10-27 08:51:02 +0100

    Add solution to task 7

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -76,3 +76,14 @@ const executeWork = (employee: any) => {
 
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
+
+// Task 7. String literal types
+
+type Subjects = "Math" | "History"
+
+const teachClass = (todayClass: Subjects): string => {
+    return todayClass === "Math"? "Teaching Math" : "Teaching History"
+}
+
+console.log(teachClass('Math'))
+console.log(teachClass('History'))
