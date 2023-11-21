f2f9b2b92bf344b421edc2e45f62ccc93dc76489
lawalgodwin <godwinl200@gmail.com>
2023-11-21 15:49:44 +0100

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,27 +5,27 @@ const countStudents = (fileName) => {
   let fileContent;
   try {
     fileContent = fs.readFileSync(path.join(__dirname, fileName), { encoding: 'utf-8' });
-  } catch{
+  } catch (error) {
     throw new Error('Cannot load the database');
-  }  
-    let lines = [];
-    for (const line of fileContent.split('\r\n')) lines = [...lines, line];
-    while (lines[lines.length - 1] === '') lines.pop();
-    lines = lines.slice(1);
-    const allFields = new Set(lines.map((line) => line.split(',')[3]));
+  }
+  let lines = [];
+  for (const line of fileContent.split('\r\n')) lines = [...lines, line];
+  while (lines[lines.length - 1] === '') lines.pop();
+  lines = lines.slice(1);
+  const allFields = new Set(lines.map((line) => line.split(',')[3]));
 
-    console.log(`Number of students: ${lines.length}`);
+  console.log(`Number of students: ${lines.length}`);
 
-    for (const field of allFields) {
-      let studentsFirstNames = [];
-      lines.map((line) => {
-        if (line.split(',')[3] === field) {
-          studentsFirstNames = [...studentsFirstNames, line.split(',')[0]];
-        }
-        return line;
-      });
-      console.log(`Number of students in ${field}: ${studentsFirstNames.length}. List: ${studentsFirstNames.join(', ')}`);
-    }
+  for (const field of allFields) {
+    let studentsFirstNames = [];
+    lines.map((line) => {
+      if (line.split(',')[3] === field) {
+        studentsFirstNames = [...studentsFirstNames, line.split(',')[0]];
+      }
+      return line;
+    });
+    console.log(`Number of students in ${field}: ${studentsFirstNames.length}. List: ${studentsFirstNames.join(', ')}`);
+  }
 };
 
 module.exports = countStudents;
