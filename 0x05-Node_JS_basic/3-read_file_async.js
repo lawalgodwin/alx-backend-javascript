d019a1a5eb8e6f8444a44f82c9e843d0e33f89d2
lawalgodwin <godwinl200@gmail.com>
2023-11-22 09:27:50 +0100

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { promisify } = require('util');
+
+const countStudents = async (fileName) => {
+  try {
+    fs.readFile = promisify(fs.readFile);
+    const data = await fs.readFile(fileName, 'utf8');
+    const lines = data.split('\r\n');
+    while (lines[lines.length - 1] === '') lines.pop();
+    const students = lines.slice(1);
+    const NUMBER_OF_STUDENTS = students.length;
+    const courses = new Set(students.map((student) => student.split(',')[3]));
+    console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+    courses.forEach((course) => {
+      let studentsFirstNames = [];
+      students.map((student) => {
+        if (student.split(',')[3] === course) {
+          studentsFirstNames = [...studentsFirstNames, student.split(',')[0]];
+        }
+        return student;
+      });
+      console.log(`Number of students in ${course}: ${studentsFirstNames.length}. List: ${studentsFirstNames.join(', ')}`);
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
