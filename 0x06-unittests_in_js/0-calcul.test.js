7aaf502e2c53f82e78c29e150b74f239a2151804
lawalgodwin <godwinl200@gmail.com>
2023-12-19 14:51:01 +0100

    Add test for calculateNumber function

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,22 +4,22 @@ const calculateNumber = require('./0-calcul');
 
 describe('Test for the function calculateNumber', () => {
   it('Addition of 1 and 3 expects 4', () => {
-    const actual = Math.ceil(calculateNumber(1, 3));
+    const actual = calculateNumber(1, 3);
     const expected = 4;
     assert.equal(actual, expected);
   });
   it('Addition of 1 and 3.7 expects 5', () => {
-    const actual = Math.ceil(calculateNumber(1, 3.7));
+    const actual = calculateNumber(1, 3.7);
     const expected = 5;
     assert.equal(actual, expected);
   });
   it('Addition of 1.2 and 3.7 expects 5', () => {
-    const actual = Math.ceil(calculateNumber(1.2, 3.7));
+    const actual = calculateNumber(1.2, 3.7);
     const expected = 5;
     assert.equal(actual, expected);
   });
   it('Addition of 1.5 and 3.7 expects 6', () => {
-    const actual = Math.ceil(calculateNumber(1.5, 3.7));
+    const actual = calculateNumber(1.5, 3.7);
     const expected = 6;
     assert.equal(actual, expected);
   });
