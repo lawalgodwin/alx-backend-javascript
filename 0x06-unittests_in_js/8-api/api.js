38a9834953c5e38c6fe8981c8ff75547fa6d9b17
lawalgodwin <godwinl200@gmail.com>
2023-12-20 10:35:08 +0100

    Add Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -4,4 +4,4 @@ app.get('/', (req, res) => {
     res.send("Welcome to the payment system")
 })
 
-app.listen(7865, () => {console.log("PI available on localhost port 7865")})
+app.listen(7865, () => {console.log("API available on localhost port 7865")})
